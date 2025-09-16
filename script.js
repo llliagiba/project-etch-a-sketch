70f6ce6c0781c88de8eb55ae618344f6dd229a95
Abigail <abigail.alemayehum@gmail.com>
2025-09-16 12:02:54 +0300

    Create 'color' functionality on mouseover

--- script.js
+++ script.js
@@ -8,11 +8,19 @@ function displayGrid() {
     for (let i = 0; i < (rows * rows); i++) {
         const gridCell = document.createElement("div");
         gridCell.style.width = gridCell.style.height = `${(gridContainerSize / rows)}px`;
-
         gridCell.classList.add("cell");
-
         sketchpad.appendChild(gridCell);
     }
 }
 
 displayGrid();
+
+const gridCells = document.querySelectorAll(".cell");
+
+gridCells.forEach((cell) => {
+    cell.addEventListener("mouseover", () => {
+        cell.classList.add("cell-color");
+        cell.classList.add("colored-cell-border");
+    });
+});
+    
