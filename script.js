fd89eb9caa7ef337b6d9efb9cd2fb38b83de90c5
Abigail <abigail.alemayehum@gmail.com>
2025-09-18 17:35:26 +0300

    Add functionality to color cells using random RGB values

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const gridContainerSize = 600;
-// let rows = 32;
 
 const sketchpad = document.querySelector(".sketchpad");
 const sliderContainer = document.querySelector(".slider-container");
 const slider = document.querySelector(".slider");
 const sliderValue = document.querySelector(".slider-value");
+const randomColorButton = document.querySelector(".random");
 
 sliderValue.textContent = `${slider.value} x ${slider.value}`;
 sketchpad.style.width = sketchpad.style.height = `${gridContainerSize}px`;
@@ -21,14 +21,34 @@ function displayGrid(rows) {
 }
 
 function colorCells() {
+    function generateRandomCode() {
+        const random = Math.floor(Math.random() * 256);
+        return random;
+    }
+
+    let randomClicked;
+    randomColorButton.addEventListener("click", () => {
+        randomClicked = true;
+    });
+
     const gridCells = document.querySelectorAll(".cell");
     gridCells.forEach((cell) => {
         cell.addEventListener("mouseover", () => {
-            cell.classList.add("cell-color");
-            cell.classList.add("colored-cell-border");
+            if (randomClicked) {
+                const red = generateRandomCode();
+                const green = generateRandomCode();
+                const blue = generateRandomCode();
+
+                cell.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 0.3)`
+            } else {
+                // might have to change this to style.backgroundColor because user should be able to PCCK a color 
+                cell.classList.add("cell-color");
+                cell.classList.add("colored-cell-border");
+            }
         });
     });
 }
+
 
 function removeGrid() {
     sketchpad.textContent = "";
