550d1df4fea1cc5e7efceb4b3721f150c1bf9cee
Abigail <abigail.alemayehum@gmail.com>
2025-09-19 05:49:16 +0300

    Add functionality to increase cell color opacity with every interaction

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ const sketchpad = document.querySelector(".sketchpad");
 const sliderContainer = document.querySelector(".slider-container");
 const slider = document.querySelector(".slider");
 const sliderValue = document.querySelector(".slider-value");
+const colorPickerButton = document.querySelector(".color");
 const randomColorButton = document.querySelector(".random");
+const shadeButton = document.querySelector(".shade");
 
 sliderValue.textContent = `${slider.value} x ${slider.value}`;
 sketchpad.style.width = sketchpad.style.height = `${gridContainerSize}px`;
@@ -20,38 +22,61 @@ function displayGrid(rows) {
     }
 }
 
+function removeGrid() {
+    sketchpad.textContent = "";
+}
+
 function colorCells() {
     function generateRandomCode() {
         const random = Math.floor(Math.random() * 256);
         return random;
     }
 
+    let colorClicked;
+    colorPickerButton.addEventListener("click", () => {
+        colorClicked = true;
+        randomClicked = false;
+        shadeClicked = false;
+    });
+
     let randomClicked;
     randomColorButton.addEventListener("click", () => {
         randomClicked = true;
+        colorClicked = false;
+        shadeClicked = false;
+    });
+
+    let shadeClicked;
+    shadeButton.addEventListener("click", () => {
+        shadeClicked = true;
+        colorClicked = false;
+        randomClicked = false;
     });
 
     const gridCells = document.querySelectorAll(".cell");
     gridCells.forEach((cell) => {
+        let opacity = 0;
         cell.addEventListener("mouseover", () => {
             if (randomClicked) {
                 const red = generateRandomCode();
                 const green = generateRandomCode();
                 const blue = generateRandomCode();
 
-                cell.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 0.3)`
+                cell.style.backgroundColor = `rgba(${red}, ${green}, ${blue}, 0.3)`;
+            } else if (shadeClicked) {
+                if (opacity > 1) {
+                    opacity = 1;
+                } else {
+                    opacity += 0.1;
+                }
+                cell.style.backgroundColor = `rgba(0, 0, 0, ${opacity})`;
             } else {
-                // might have to change this to style.backgroundColor because user should be able to PCCK a color 
+                // might have to change this to style.backgroundColor because user should be able to PICK a color 
                 cell.classList.add("cell-color");
                 cell.classList.add("colored-cell-border");
             }
         });
     });
-}
-
-
-function removeGrid() {
-    sketchpad.textContent = "";
 }
 
 // New grid based on slider value
@@ -66,3 +91,4 @@ slider.addEventListener("input", () => {
 // Initial grid: on page load
 displayGrid(16);
 colorCells();
+
